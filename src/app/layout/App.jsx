77778a30d94e176f9904b897c05c892c5f842c68
Philip Nguyen <vietwhammies@gmail.com>
2018-09-01 23:10:01 -0700

    Initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+// This gets rendered onto our page, injected
+// in place of content in index.html
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Re-vents</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
