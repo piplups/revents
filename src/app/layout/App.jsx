33a70d7914f5d49bba76278ac11161d049cb52a3
Philip Nguyen <vietwhammies@gmail.com>
2018-09-02 22:55:28 -0700

    added event lists as a feature

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react'
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar'
 
 // This gets rendered onto our page, injected
 // in place of content in index.html
@@ -6,9 +9,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Re-vents</h1>
+        <NavBar/>
+        <Container className="main">  
+          <EventDashboard/>
+        </Container>
       </div>
-        <EventDashboard/>
     );
   }
 }
