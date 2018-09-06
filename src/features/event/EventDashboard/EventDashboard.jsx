ad55fb22f4eabf85d33678f2f070cc66b8de138f
Philip Nguyen <vietwhammies@gmail.com>
2018-09-06 15:16:14 -0700

    can now successfully create, update and delete events

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -8,7 +8,7 @@ const eventsDashboard = [
   {
     id: '1',
     title: 'Trip to Tower of London',
-    date: '2018-03-27T11:00:00+00:00',
+    date: '2018-03-27',
     category: 'culture',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
@@ -32,7 +32,7 @@ const eventsDashboard = [
   {
     id: '2',
     title: 'Trip to Punch and Judy Pub',
-    date: '2018-03-28T14:00:00+00:00',
+    date: '2018-03-28',
     category: 'drinks',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
@@ -59,13 +59,9 @@ const eventsDashboard = [
 class EventDashboard extends Component {
   state = {
     events: eventsDashboard,
-    isOpen: false
-
-    //Inverse dataflow: These binds the functions to the state
-    //Binds all event handlers in the constructor
-    //this.handleFormOpen = this.handleFormOpen.bind(this);
-    //this.handleCancel = this.handleCancel.bind(this);
-  }
+    isOpen: false,
+    selectedEvent: null
+  };
 
   //opens the form once "create event" is clicked on RH side
   handleFormOpen = () =>{
@@ -87,20 +83,52 @@ class EventDashboard extends Component {
     this.setState ({
       events: updatedEvents,
       isOpen: false
-    })
-  }
+    });
+  };
+
+  handleUpdateEvent = (updatedEvent) => {
+    this.setState({
+      events: this.state.events.map(event => {
+        if (event.id === updatedEvent.id) {
+          return Object.assign({}, updatedEvent);
+        } else {
+          return event
+        }
+      }),
+      isOpen: false,
+      selectedEvent: null
+    });
+  };
+
+  // In lecture 37: Selecting Event to Open
+  // get data from eventlistitem arrow function into arrow function
+  // double arrow is so that we don't have to make one in the render method
+  handleOpenEvent = (eventToOpen) => () => {
+    this.setState({
+      selectedEvent: eventToOpen,
+      isOpen: true
+    });
+  };
+
+  handleDeleteEvent = (eventId) => () => {
+    const updatedEvents = this.state.events.filter(e => e.id !== eventId);
+    this.setState({
+      events: updatedEvents
+    });
+  };
 
   render() {
+    const {selectedEvent} = this.state;
     //using  x && y is the same thing as if x, then y
     return (
       <Grid>
         <Grid.Column width={10}>
-            <EventList events={this.state.events}/>
+            <EventList deleteEvent={this.handleDeleteEvent} onEventOpen={this.handleOpenEvent} events={this.state.events}/>
         </Grid.Column>
         <Grid.Column width={6}>
           <Button onClick={this.handleFormOpen} positive content='Create Event'/>
             {this.state.isOpen && 
-            <EventForm createEvent={this.handleCreateEvent} handleCancel={this.handleCancel}
+            <EventForm updateEvent={this.handleUpdateEvent} selectedEvent={selectedEvent} createEvent={this.handleCreateEvent} handleCancel={this.handleCancel}
             />}
         </Grid.Column>
       </Grid>
