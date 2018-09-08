bd729fc18805aa764f665865a4c54a19d9492c1d
Philip Nguyen <vietwhammies@gmail.com>
2018-09-07 17:19:50 -0700

    multiple links now work and boolean login system works

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react'
-import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
-import NavBar from '../../features/nav/NavBar/NavBar'
+import { Container } from 'semantic-ui-react';
+import { Route, Switch } from 'react-router-dom';
+import EventDashboard from    '../../features/event/EventDashboard/EventDashboard';
+import NavBar from            '../../features/nav/NavBar/NavBar';
+import EventForm from         '../../features/event/EventForm/EventForm';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+import PeopleDashboard from   '../../features/user/PeopleDashboard/PeopleDashboard';
+import UserDetailedPage from  '../../features/user/UserDetailed/UserDetailedPage';
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
+import HomePage from          '../../features/home/HomePage';
 
 // This gets rendered onto our page, injected
 // in place of content in index.html
@@ -9,10 +16,29 @@ class App extends Component {
   render() {
     return (
       <div>
-        <NavBar/>
-        <Container className="main">  
-          <EventDashboard/>
-        </Container>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      <Route 
+        path="/(.+)" 
+        render={() => (
+          <div>
+            <NavBar/>
+            <Container className="main">  
+              <Switch>
+                <Route path='/events' component={EventDashboard}/>
+                <Route path='/events/:id' component={EventDetailedPage}/>
+                <Route path='/people' component={PeopleDashboard}/>
+                <Route path='/profile/:id' component={UserDetailedPage}/>
+                <Route path='/settings' component={SettingsDashboard}/>
+                <Route path='/createEvent' component={EventForm}/>
+              </Switch>
+              
+              </Container>
+            </div>
+          )}
+        />
+          
       </div>
     );
   }
---
+++ src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PeopleDashBoard = () => {
+    return (
+        <div>
+            <h1>People DashBoard Page</h1>
+        </div>
+    )
+}
+
+export default PeopleDashBoard
