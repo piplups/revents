92c421abb7818f08a2f43ae53b4bf14de84b8655
Philip Nguyen <vietwhammies@gmail.com>
2018-09-12 11:12:16 -0700

    redux and state management are implemented on test page

---
+++ src/app/common/util/reducerUtil.js
@@ -0,0 +1,8 @@
+export const createReducer = (intialState, fnMap) => {
+    return (state = intialState, {type, payload}) => {
+        const handler = fnMap[type];
+
+        return handler ? handler(state, payload): state
+
+    }
+}
--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -9,6 +9,7 @@ import PeopleDashboard from   '../../features/user/PeopleDashboard/PeopleDashboa
 import UserDetailedPage from  '../../features/user/UserDetailed/UserDetailedPage';
 import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
 import HomePage from          '../../features/home/HomePage';
+import TestComponent from     '../../features/testarea/TestComponent'
 
 // This gets rendered onto our page, injected
 // in place of content in index.html
@@ -27,6 +28,7 @@ class App extends Component {
             <Container className="main">  
               <Switch>
                 <Route path='/events' component={EventDashboard}/>
+                <Route path='/test' component={TestComponent}/>
                 <Route path='/events/:id' component={EventDetailedPage}/>
                 <Route path='/people' component={PeopleDashboard}/>
                 <Route path='/profile/:id' component={UserDetailedPage}/>
